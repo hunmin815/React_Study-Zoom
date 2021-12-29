a89d6a9eff2081dee84369a00a04eea2d666e9b5
hunmin815 <hunmin815@naver.com>
2021-12-30 01:46:52 +0900

    Day 4 : Create React & NodeJS Socket Link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SocketIO from "socket.io-client";
+import { useState } from "react";
+
+const sockIO = SocketIO.connect("http://localhost:3333");
+
+// sockIO.emit("enter_room", { payload: "jo" });
 
 function App() {
+  let [RoomName, RoomName_C] = useState("");
+
+  const handelInputRoomName = (e) => {
+    RoomName_C(e.target.value);
+  };
+
+  const EnterRoom = (e) => {
+    e.preventDefault();
+    sockIO.emit("enter_room", { payload: RoomName });
+    sockIO.on("Hello", (data) => {
+      console.log(`Server : ${data}`);
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>{RoomName}</h3>
+        <form id="form_Room">
+          <input
+            type="text"
+            placeholder="Room Name"
+            required
+            onChange={handelInputRoomName}
+          ></input>
+          <button onClick={EnterRoom}>Enter room</button>
+        </form>
       </header>
     </div>
   );
